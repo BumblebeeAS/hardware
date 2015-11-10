6c6dbde22fdf073efea979fc0b94102662ecd32e
Goh Eng Wei <gohengwei@gmail.com>
2015-11-10 16:24:55 +0800

    updated  batch script

--- Scripts/uploader.cmd
+++ Scripts/uploader.cmd
@@ -1,5 +1,4 @@
 echo off
-echo test batch
 pscp -pw password C:\Users\gohew\AppData\Local\V.Micro\Arduino\Builds\can_send_bandwidth\uno\can_send_bandwidth.hex gohew@192.168.1.34:mcu.hex
 
 if "%1"=="mega2560" plink -pw password gohew@192.168.1.34 avrdude -p atmega2560 -b 115200 -c stk500v2 -P/dev/%2 -U flash:w:mcu.hex
