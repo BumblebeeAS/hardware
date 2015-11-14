fd288b06af3ac5be900556e44517e9d9b00ad85d
Goh Eng Wei <gohengwei@gmail.com>
2015-11-14 17:22:22 +0800

    Updated uploader script and SA libraries format

--- Scripts/uploader.cmd
+++ Scripts/uploader.cmd
@@ -1,8 +1,9 @@
-echo off
-pscp -pw password C:\Users\gohew\AppData\Local\V.Micro\Arduino\Builds\can_send_bandwidth\uno\can_send_bandwidth.hex gohew@192.168.1.34:mcu.hex
+###echo off
+pscp -pw bb %3 bbauv@192.168.1.130:mcu.hex
 
-if "%1"=="mega2560" plink -pw password gohew@192.168.1.34 avrdude -p atmega2560 -b 115200 -c stk500v2 -P/dev/%2 -U flash:w:mcu.hex
+if "%1"=="mega2560" plink -pw bb bbauv@192.168.1.130 avrdude -C "/etc/avrdude.conf" -v -patmega2560 -cwiring -P/dev/%2 -b115200 -D -Uflash:w:mcu.hex:i
 
-if "%1"=="mega328p" plink -pw password gohew@192.168.1.34 avrdude -p atmega328p -b 115200 -c stk500v2 -P/dev/%2 -U flash:w:mcu.hex
+if "%1"=="mega328p" plink -pw bb bbauv@192.168.1.130 avrdude -p atmega328p -b 115200 -c stk500v2 -P/dev/%2 -U flash:w:mcu.hex
 
 
+avrdude -C "/etc/avrdude.conf" -v -patmega2560 -cwiring -P/dev/%2 -b115200 -D -Uflash:w:mcu.hex:i
