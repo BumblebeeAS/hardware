45b508cdd020829966c614073f7adc13bf90bbe6
Fan Jiaming <quakeiii2012@gmail.com>
2016-09-18 21:01:09 +0800

    Tidy up the main code, it largely resembles the test code

--- ASV/Manipulator/main code/Manipulator.ino
+++ ASV/Manipulator/main code/Manipulator.ino
@@ -1,30 +1,25 @@
-#include <MANIPULATOR.h>
+#include <Servo.h>
+#include <Wire.h>
+#include "IMU.h"
+#include "Sweeper.h"
 
-SWEEPER sweep1(5);
+Sweeper sweep(5);
 IMU imu(0x68);
 
 void setup(){
   Serial.begin(115200);
-  imu.Init();
+  imu.init();
   imu.correctToZero();          // run only when fitting to new position
-  sweep1.Attach(9);             // pin connection
+  sweep.attach(9);             // pin connection
 }
 
 void loop(){
-  int target_us=0,diff=0;
   
+  int target_us = 0,diff = 0;
+ 
   imu.readAccTempGyro();
-//  imu.printAccTempGyro();
-
-  diff =(round)(975/90*imu.Error()+1525);
-  
-  sweep1.Update(target_us,diff);
-
-  
-//  imu.setting(); 
-//  imu.CalibrateAcc();
-//  imu.printAccTempGyro();
-   
+  diff = (round)(975 / 90 * imu.correctError() + 1525);
+  sweep.update(target_us, diff);
 }
 
 
