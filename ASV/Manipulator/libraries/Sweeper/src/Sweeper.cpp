fe74bcc62fd9ecaae99c7634679ea2e4c638affc
Fan Jiaming <quakeiii2012@gmail.com>
2016-09-18 20:54:42 +0800

    Separated libraries according to class name

---
+++ ASV/Manipulator/libraries/Sweeper/src/Sweeper.cpp
@@ -0,0 +1,28 @@
+#include "Sweeper.h"
+
+/**************************
+*	START OF PUBLIC METHOD
+***************************/
+
+Sweeper::Sweeper(int interval) {
+	updateInterval = interval;
+}
+
+void Sweeper::attach(int pin) {
+	servo.attach(pin);
+}
+
+void Sweeper::detach(void) {
+	servo.detach();
+}
+
+void Sweeper::update(int target_us, int error) {
+	if ((millis() - lastUpdate) > updateInterval) {
+		lastUpdate = millis();
+		servo.writeMicroseconds(target_us + error);
+	}
+}
+
+/**************************
+*	END OF PUBLIC METHOD
+***************************/
