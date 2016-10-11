dd7421f4617cc35185241ec3bbe91cd20e3ea501
yk6 <grantex5@gmail.com>
2016-10-11 10:33:32 +0800

    add initial position and limiting angle of turning

--- ASV/Manipulator/libraries/Sweeper/src/Sweeper.cpp
+++ ASV/Manipulator/libraries/Sweeper/src/Sweeper.cpp
@@ -1,5 +1,7 @@
 #include "Sweeper.h"
 
+#define HORIZONTAL 2250    // initial flat position at 157deg  ( 0-180 scale )
+
 /**************************
 *	START OF PUBLIC METHOD
 ***************************/
@@ -10,6 +12,7 @@ Sweeper::Sweeper(int interval) {
 
 void Sweeper::attach(int pin) {
 	servo.attach(pin);
+	servo.writeMicroseconds(HORIZONTAL);
 }
 
 void Sweeper::detach(void) {
@@ -17,9 +20,16 @@ void Sweeper::detach(void) {
 }
 
 void Sweeper::update(int target_us, int error) {
+	int move=target_us+error;						//2456 is max backward------------ 176 deg
+	if (move>2456) {								//2250 is flat---------------------157 deg
+		move=2456;									//1980 is max forward--------------132 deg
+	}
+	if (move<1980) {
+		move=1980;
+	}
 	if ((millis() - lastUpdate) > updateInterval) {
 		lastUpdate = millis();
-		servo.writeMicroseconds(target_us + error);
+		servo.writeMicroseconds(move);
 	}
 }
 
