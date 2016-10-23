299ffb6ab802d1b7a0fec56adef60781ab4ab0b1
yk6 <grantex5@gmail.com>
2016-10-24 01:32:10 +0800

    added more test code

---
+++ ASV/Manipulator/test code/switch/switch.ino
@@ -0,0 +1,16 @@
+int SWA = 22;
+
+void setup (void) {
+  Serial.begin(115200);
+  pinMode(SWA,INPUT);
+}
+
+void loop(void) {
+  if (digitalRead(SWA)==LOW) {
+    Serial.println("LOW");
+  } else {
+    Serial.println("HIGH");
+  }
+  delay(100);
+}
+
