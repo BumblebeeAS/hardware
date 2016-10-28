c17694c3c3cfe7bd0a9b7c6fd0b4fd4728a34787
Fan Jiaming <quakeiii2012@gmail.com>
2016-10-28 15:19:39 +0800

    added in shooter control and deleted actuator collision detection, actuators need to specifically turn off except shooter

--- ASV/Manipulator/libraries/Actuator/src/Actuator.h
+++ ASV/Manipulator/libraries/Actuator/src/Actuator.h
@@ -14,33 +14,26 @@
 
 #define ROTARY_DEFAULT		150
 #define LINEAR_DEFAULT		50
-#define SHOOTER_DEFAULT		500
-
-enum ActuationStatusType : uint8_t {
-	ROTATED = 0x00,
-	ROTATE_EXTENDED,
-	REST
-};
+#define SHOOTER_DEFAULT		10
 
 enum ActuationType : uint8_t {
-	ROTARY = 0x00,
-	LINEAR,
-	SHOOT
+	ROTATE_DOWN = 0x00,
+	ROTATE_UP,
+	LINEAR
 };
 
 struct _Actuator {
-	uint8_t pin;	//corresponding pin at the IO expander
-	uint16_t time;	//corresponding actuation time
-	uint16_t currTime; //time which the counter starts
-	bool requestToDisconnect;	//boolean to determine the actuation should stop
+	uint8_t pin;				//corresponding pin at the IO expander
+	uint16_t time;				//corresponding actuation time
+	uint16_t currTime; 			//time which stores the current time value
+	bool requestToDisconnect;	//flag to determine if the actuator needs to stop actuating
 };
 
 class Actuator
 {
  private:
 	 MCP23S17 mcp23s17 = { MCP_ADDR, CS_IO };
-	 ActuationStatusType actuationStatus;
-	 _Actuator rotary_deploy, rotary_retrieve, linear, shooter[4];
+	 _Actuator rotateUp, rotateDown, linear, shooter[4];
 
  public:
 
@@ -49,16 +42,16 @@ class Actuator
 	 
 	 void init(void);
 
-	 void deployAcoustic(ActuationType actuationType);
-	 void retrieveAcoustic(ActuationType actuationType);
+	 //linear and rotary actuator needs to be specifically turn off
+	 void actuateAcoustic(ActuationType actuationType);
+	 void stopActuation(ActuationType actuationType);
 	 
+	 //shooter will be automatically turn off using timer 1
 	 void actuateShooter(uint8_t num);
 
 	 //call back function to switch off the actuation
 	 void actuatorCallBack(void);
 };
-
-extern Actuator actuator;
 
 #endif
 
