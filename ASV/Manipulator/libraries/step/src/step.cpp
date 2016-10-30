ee3ccae0f9c59b56466786839ef53f05c24db4f1
yk6 <grantex5@gmail.com>
2016-10-31 07:42:03 +0800

    stepper pin

--- ASV/Manipulator/libraries/step/src/step.cpp
+++ ASV/Manipulator/libraries/step/src/step.cpp
@@ -9,10 +9,10 @@
 
 
 Step::Step(void) {
-	inA1 = 2; // input 1 of the stepper  
-    inA2 = 4; // input 2 of the stepper
-    inB1 = 3; // input 3 of the stepper
-    inB2 = 5; // input 4 of the stepper
+	inA1 = 23; // input 1 of the stepper  
+    inA2 = 25; // input 2 of the stepper
+    inB1 = 24; // input 3 of the stepper
+    inB2 = 26; // input 4 of the stepper
 /*
     correct mapping on manipulator board 
     23			PC0			STPA1		
