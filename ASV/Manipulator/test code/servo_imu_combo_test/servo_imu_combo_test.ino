c04b9329f6eb2251ea7b79ee298aa218a29fc7b5
yk6 <grantex5@gmail.com>
2016-11-13 21:10:39 +0800

    test code

--- ASV/Manipulator/test code/servo_imu_combo_test/servo_imu_combo_test.ino
+++ ASV/Manipulator/test code/servo_imu_combo_test/servo_imu_combo_test.ino
@@ -9,22 +9,24 @@ uint32_t looptime = 0;
 
 Sweeper sweep(1);
 
+
 void setup() {
   // put your setup code here, to run once:
   imu.init();
   sweep.attach(10);
   Serial.begin(115200);
+  sweep.enable = 1;
 }
 
 void loop() {
 //   put your main code here, to run repeatedly:
 
   imu.readAccTempGyro();  
-  int target = 19;
+  int target = 15;
   target -= imu.correctError();
   target = map(target,0,38,1980,2456); 
 
-  Serial.println(target);
+//  Serial.println(target);
   
   sweep.update(target);
     
--- ASV/Manipulator/test code/stepper/stepper.ino
+++ ASV/Manipulator/test code/stepper/stepper.ino
@@ -15,29 +15,29 @@ void setup(void) {
   a.Init();
 //  pinMode(SWA,INPUT);
   a.enable = 1;
-  a.target = 9;
+  a.target = 30;
 }
 
 void loop(void) {
 //  
-//  for(int i=0; i<=305; i++) {
-//    a.step1();
-//    a.step2();
+//  for(int i=0; i<=305; i++) {                 // basic code of movement,  possible reason of not moving: pin config wrong ,
+//    a.step1();                                //                                                         limit switch stopping u   ,      
+//    a.step2();                                //                                                         motor chip died
 //    a.step3();
 //    a.step4();
 //  }
 //  a.stopMotor();
 //  delay(1000);
 //-------------------------------------------
-//  a.moveLeft();
+//  a.moveLeft();                               // limit switch or chip died
 //------------------------------------------
 //  a.moveRight();
 //  delay(5);
 //------------------------------------------
-//Serial.println(a.checkLimit());
-    a.moveStepper();
-//    delay(100);
-//Serial.println(a.target);  
+//Serial.println(a.checkLimit());               //  1 check is it due to the limit switch causing stepper not to move    
+    a.moveStepper();                            //  not enabled, ,                                
+//    delay(100);                               //  it thinks it is at the wrong position(out of range) 
+//Serial.println(a.target);                     // chip died,   limit switch
 }
 
 
