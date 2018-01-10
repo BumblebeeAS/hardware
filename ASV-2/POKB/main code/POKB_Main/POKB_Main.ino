5ba87bf87ba64764be005253ab503435027409ef
draker789 <ren_jie15@hotmail.com>
2018-01-10 09:21:43 -0500

    ASV2 POKB: Added CAN

--- ASV-2/POKB/main code/POKB_Main/POKB_Main.ino
+++ ASV-2/POKB/main code/POKB_Main/POKB_Main.ino
@@ -6,6 +6,13 @@
 
 #include "XBee.h"
 #include "defines.h"
+#include "can_asv_defines.h"
+#include <can.h>
+#include <can_defines.h>
+
+MCP_CAN CAN(CAN_Chip_Select);               //Set Chip Select to pin 8
+unsigned char len = 0; //length of CAN message, taken care by library
+uint8_t buf[8];  //Buffer for CAN message
 
 XBee xbee = XBee();
 
@@ -15,6 +22,8 @@ ZBRxResponse rx = ZBRxResponse();
 ModemStatusResponse msr = ModemStatusResponse();
 
 static uint32_t xbee_loop = 0;
+static uint32_t heartbeat_loop = 0;
+
 uint8_t incomingByte = 0;
 int no_data = 0;
 bool software_kill = 0;
@@ -24,35 +33,40 @@ void setup() {
 	Serial.begin(SERIAL_BAUDRATE);
 	Serial1.begin(XBEE_BAUDRATE);
 	xbee.setSerial(Serial1);
-	pinMode(11, OUTPUT);
-	digitalWrite(11, LOW);
-
-	pinMode(5, OUTPUT);
-	digitalWrite(5, LOW);
-
+	pinMode(NMOS_CONTACTOR, OUTPUT);
+	digitalWrite(NMOS_CONTACTOR, LOW);
+	
 	Serial.println("reset");
 
+	CAN_init();
+	Serial.println("CAN OK");
+
 	xbee_loop = millis();
+	heartbeat_loop = millis();
 }
 
 void loop() {
-	if ((millis() - xbee_loop) > XBEE_DELAY){
+	if ((millis() - xbee_loop) > XBEE_TIMEOUT){
 		xbee_receive();
 		xbee_loop = millis();
+	}
+
+
+	if ((millis() - heartbeat_loop) > HEARTBEAT_TIMEOUT) {
+		publishCAN_heartbeat();
+		heartbeat_loop = millis();
 	}
 }
 
 void On_Contactor() {
-	digitalWrite(11, HIGH);
-	digitalWrite(5, HIGH);
+	digitalWrite(NMOS_CONTACTOR, HIGH);
 	Serial.println("normal");
 	software_kill = 0;
 	no_data = 0;
 }
 
 void Kill_Contactor() {
-	digitalWrite(11, LOW);
-	digitalWrite(5, LOW);
+	digitalWrite(NMOS_CONTACTOR, LOW);
 	Serial.println("KILL!");
 	software_kill = 1;
 	no_data = 0;
@@ -72,10 +86,12 @@ void xbee_receive(){
 
 			if (incomingByte == 0x15) {
 				On_Contactor();
+				publishCAN_estop(false);
 			}
 
 			else {
 				Kill_Contactor();
+				publishCAN_estop(true);
 			}
 		}
 	}
@@ -91,4 +107,36 @@ void xbee_receive(){
 		}
 		Serial.println("Nothing Available");
 	}
+}
+
+
+//========== CAN ==========//
+
+void CAN_init() {
+START_INIT:
+	if (CAN_OK == CAN.begin(CAN_1000KBPS)) {                   // init can bus : baudrate = 500k
+#if DEBUG_MODE == NORMAL
+		Serial.println("CAN init: OK");
+#endif           
+	}
+	else {
+#if DEBUG_MODE == NORMAL
+		Serial.println("CAN init: FAILED");
+		Serial.println("reinitialising CAN...");
+		delay(1000);
+#endif           
+		goto START_INIT;
+	}
+}
+
+void publishCAN_heartbeat()
+{
+	CAN.setupCANFrame(buf, 0, 1, HEARTBEAT_POKB);
+	CAN.sendMsgBuf(CAN_heartbeat, 0, 1, buf);
+}
+
+void publishCAN_estop(bool estop_status)
+{
+	CAN.setupCANFrame(buf, 0, 1, estop_status);
+	CAN.sendMsgBuf(CAN_e_stop, 0, 1, buf);
 }
--- ASV-2/POKB/main code/POKB_Main/defines.h
+++ ASV-2/POKB/main code/POKB_Main/defines.h
@@ -1,11 +1,19 @@
 #ifndef _DEFINES_H_
 #define _DEFINES_H_
 
+// CAN
+#define CAN_Chip_Select 8
+
+// KILL
 #define NMOS_CONTACTOR 11
 
+
+// XBEE
 #define XBEE_BAUDRATE 115200
 #define SERIAL_BAUDRATE 115200
 
-#define XBEE_DELAY 500
+// TIMEOUT
+#define XBEE_TIMEOUT 500
+#define HEARTBEAT_TIMEOUT 500
 
 #endif
