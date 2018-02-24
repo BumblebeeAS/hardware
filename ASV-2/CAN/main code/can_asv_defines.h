ea4ef1041465226be4e143cedab6e79ef8ad47ca
zhijietan <zhijietan94@gmail.com>
2018-02-25 03:42:34 +0800

    ASV2_CAN: Updated can_asv_defines.h to include software-estop

--- ASV-2/CAN/main code/can_asv_defines.h
+++ ASV-2/CAN/main code/can_asv_defines.h
@@ -23,26 +23,27 @@
 #define CAN_manual_thruster	102
 #define CAN_control_link	103
 #define CAN_heartbeat	104
-#define CAN_e_stop		105
-#define CAN_wind_speed	106
-#define CAN_POPB_control	107
-#define CAN_LARS 108
-#define CAN_light_tower 109
-#define CAN_battery1_motor_stats 110
-#define CAN_battery2_motor_stats 111
-#define CAN_esc1_motor_stats 112
-#define CAN_esc2_motor_stats 113
-#define CAN_remote_kill_stats 114
-#define CAN_INS_stats 115
-#define CAN_GPS_stats 116
-#define CAN_cpu_temp 117
-#define CAN_POSB_stats 118
-#define CAN_POPB_stats 119
-#define CAN_POSB_BUS_stats 120
-#define CAN_POKB_BUS_stats 121
-#define CAN_POPB_BUS_stats 122
-#define CAN_Tele_BUS_stats 123
-#define CAN_LARS_BUS_stats 124
+#define CAN_soft_e_stop 105
+#define CAN_e_stop		106
+#define CAN_wind_speed	107
+#define CAN_POPB_control	108
+#define CAN_LARS 109
+#define CAN_light_tower 110
+#define CAN_battery1_stats 111
+#define CAN_battery2_stats 112
+#define CAN_esc1_motor_stats 113
+#define CAN_esc2_motor_stats 114
+#define CAN_remote_kill_stats 115
+#define CAN_INS_stats 116
+#define CAN_GPS_stats 117
+#define CAN_cpu_temp 118
+#define CAN_POSB_stats 119
+#define CAN_POPB_stats 120
+#define CAN_POSB_BUS_stats 121
+#define CAN_POKB_BUS_stats 122
+#define CAN_POPB_BUS_stats 123
+#define CAN_Tele_BUS_stats 124
+#define CAN_LARS_BUS_stats 125
 
 //CAN Heartbeat
 #define HEARTBEAT_POSB 1
@@ -53,5 +54,10 @@
 #define HEARTBEAT_Cogswell 6
 #define HEARTBEAT_OCS 7
 #define HEARTBEAT_RC  8
+//Bit position
+#define HEARTBEAT_BATT1 0
+#define HEARTBEAT_BATT2 1
+#define HEARTBEAT_ESC1 2
+#define HEARTBEAT_ESC2 3
 
 #endif
