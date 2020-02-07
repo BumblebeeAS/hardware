0f4623fccefb435d430ce796a438cea9d2a60546
YANG YIHANG <48911420+Auliter@users.noreply.github.com>
2020-02-07 19:23:02 +0800

    Update st_can.c

--- AUV-4/STB/Src/st_can.c
+++ AUV-4/STB/Src/st_can.c
@@ -1,12 +1,13 @@
 #include "st_can.h"
 
-uint8_t CAN_recvMsg_buff[32] = {0};	//CAN recvmsgbuffer
-uint32_t RxFifo = 10;
+//uint8_t CAN_recvMsg_buff[32] = {0};	//CAN recvmsgbuffer
+//uint32_t RxFifo = 10;
 uint32_t can_publish_loop_gyro=10;
 uint32_t can_publish_loop_mag=10;
 uint32_t can_publish_loop_acc=10;
 uint32_t can_publish_loop_sens=10;
 uint32_t can_publish_loop_uptime=0;
+uint32_t check_hb_loop=0;
 
 uint32_t can_heartbeat_loop=0;
 uint8_t CAN_SEND=0;
@@ -40,8 +41,8 @@ void publishCAN_message(void){
 	}
 
 void publishCAN_heartbeat(void){
-	if(HAL_GetTick()-can_heartbeat_loop>100){
-		uint8_t Msg[1]={HEARTBEAT_Telemetry};
+	if( (HAL_GetTick()-can_heartbeat_loop)>100){
+		uint8_t Msg[1]={HEARTBEAT_STB};
 		CAN_SendMsg(CAN_HEARTBEAT,Msg,1);
 		can_heartbeat_loop=HAL_GetTick();
 	}
@@ -51,60 +52,102 @@ void publishCAN_uptime(void){
 	if(HAL_GetTick()-can_publish_loop_uptime>1000){
 		uptime=uptime+1;
 		uint8_t MSG[3]={uptime>>8,uptime,signature};
-		CAN_SendMsg(CAN_STB_STAT,MSG,1);
+		CAN_SendMsg(CAN_STB_STAT,MSG,3);
 		can_publish_loop_uptime=HAL_GetTick();
 	}
 }
 
 void update_can(void){
-	RxFifo=CAN_CheckReceive();
-	if(RxFifo!=10){
-		CAN_RecvMsg(RxFifo,CAN_recvMsg_buff);
-		uint16_t CAN_id =(uint16_t) CAN_GetId();
+	if( CAN_RP == CAN_WP){
+		return ;
+	}
+
+	while(CAN_RP != CAN_WP){
+
+	uint8_t CAN_msg[16] = {0};
+	uint8_t CAN_id = CAN_recvMsgBuf[CAN_RP], CAN_size = CAN_recvMsgBuf[CAN_RP+1];
+	uint8_t i;
+	for ( i = 0 ; i < CAN_size ; i++){
+		CAN_msg[i] = CAN_recvMsgBuf[CAN_size + CAN_RP + 2];
+	}
+
+
 		switch(CAN_id){
 		case CAN_HEARTBEAT:{
-			uint8_t device= CAN_recvMsg_buff[0];
+			uint8_t device= CAN_msg[0];
 			boardHB_timeout[device]=HAL_GetTick();
 			break;
 		}
+//		case 6:{
+//			set_cursor(500,0);
+//			textTransparent(WHITE);
+//			write_string("SBC-CAN IS OK");
+//
+//			boardHB_timeout[2]=HAL_GetTick();
+//			break;
+//		}
 		case CAN_BATT1_STAT:{
-			powerStats[BATT1_CURRENT]=(CAN_recvMsg_buff[1]<<8|CAN_recvMsg_buff[0]);
-			powerStats[BATT1_VOLTAGE]=(CAN_recvMsg_buff[3]<<8|CAN_recvMsg_buff[2]);
+			powerStats[BATT1_CURRENT]=(CAN_msg[1]<<8|CAN_msg[0]);
+			powerStats[BATT1_VOLTAGE]=(CAN_msg[3]<<8|CAN_msg[2]);
 			break;
 		}
 		case CAN_PMB1_STAT:{
-			internalStats[PMB1_PRESS]=CAN_recvMsg_buff[4];
-			internalStats[PMB1_TEMP]=CAN_recvMsg_buff[3];
-			powerStats[BATT1_CAPACITY]=CAN_recvMsg_buff[2];
+			internalStats[PMB1_PRESS]=CAN_msg[4];
+			internalStats[PMB1_TEMP]=CAN_msg[3];
+			powerStats[BATT1_CAPACITY]=CAN_msg[2];
 			break;
 		}
 		case CAN_BATT2_STAT:{
-			powerStats[BATT2_CURRENT]=(CAN_recvMsg_buff[1]<<8|CAN_recvMsg_buff[0]);
-			powerStats[BATT2_VOLTAGE]=(CAN_recvMsg_buff[3]<<8|CAN_recvMsg_buff[2]);
+			powerStats[BATT2_CURRENT]=(CAN_msg[1]<<8|CAN_msg[0]);
+			powerStats[BATT2_VOLTAGE]=(CAN_msg[3]<<8|CAN_msg[2]);
 			break;
 		}
 		case CAN_PMB2_STAT:{
-			internalStats[PMB2_PRESS]=CAN_recvMsg_buff[4];
-			internalStats[PMB2_TEMP]=CAN_recvMsg_buff[3];
-			powerStats[BATT2_CAPACITY]=CAN_recvMsg_buff[2];
+			internalStats[PMB2_PRESS]=CAN_msg[4];
+			internalStats[PMB2_TEMP]=CAN_msg[3];
+			powerStats[BATT2_CAPACITY]=CAN_msg[2];
 			break;
 		}
 		case CAN_STB_CONFIG:{
 			for (uint8_t i=0;i<STB_UP_COUNT;i++){
-				stbUP[i]=CAN_recvMsg_buff[i];
+				stbUP[i]=CAN_msg[i];
 			}
+			set_led(stbUP[LED]);
 			uint8_t MSG[8]={stbUP[7],stbUP[6],stbUP[5],stbUP[4],stbUP[3],stbUP[2],stbUP[1],stbUP[0]};
 			CAN_SendMsg(CAN_STB_UP,MSG,8);
+			break;
+		}
+		default:
+			return;
+		}
+//		set_cursor(460,400);
+//		textTransparent(WHITE);
+//		write_value_int(CAN_id);
+
+
+		CAN_RP += (2+CAN_size);
+	}
+	//update screen
+	if(HAL_GetTick()-check_hb_loop>1000){
+	for (uint8_t i=1;i<5;i++){
+		if((HAL_GetTick()-boardHB_timeout[i])>HB_TIMEOUT){
+		boardHB[i-1]=0xFFFF;
+		}
+	else{
+		boardHB[i-1]=0x1111;
 		}
 	}
-		for (uint8_t i=0;i<HB_COUNT;i++){
-			if(boardHB_timeout[i]>HB_TIMEOUT){
-			boardHB[i]=0xFFFF;
-			}
-		else{
-			boardHB[i]=0x1111;
-			}
+	for (uint8_t i=6;i<9;i++){
+		if((HAL_GetTick()-boardHB_timeout[i])>HB_TIMEOUT){
+		boardHB[i-1]=0xFFFF;
 		}
+	else{
+		boardHB[i-1]=0x1111;
+		}
+	}
+	check_hb_loop=HAL_GetTick();
 	}
 }
+
+
 
