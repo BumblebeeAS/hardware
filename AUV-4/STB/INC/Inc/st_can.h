ff54aa0d4c9fd16bee439a35b553472004b415ec
YANG YIHANG <48911420+Auliter@users.noreply.github.com>
2020-02-07 19:20:22 +0800

    Update st_can.h

--- AUV-4/STB/INC/Inc/st_can.h
+++ AUV-4/STB/INC/Inc/st_can.h
@@ -8,6 +8,8 @@
  extern "C" {
 #endif
 
+
+#include "main.h"
 #include "stm32f0xx_hal.h"			//systick, delay,...
 #include "stm32f0xx_hal_spi.h"		//can library
 #include "stm32f0xx_hal_cortex.h"	//nvic
@@ -27,38 +29,38 @@
 #define CAN_STB_CONFIG 7
 #define CAN_PMB1_CONFIG 8
 #define CAN_PMB2_CONFIG 9
-#define CAN_ALL_CONFIG 10
-#define CAN_STB_SENS 11
-#define CAN_STB_UP 12
-#define CAN_STB_GYRO 13
-#define CAN_STB_ACC 14
-#define CAN_STB_MAG 15
-#define CAN_STB_STAT 16
-#define CAN_THT_STAT_1 17
-#define CAN_THT_STAT_2 18
-#define CAN_THT_PWM_1 19
-#define CAN_THT_PWM_2 20
-#define CAN_ACT_STAT 21
-#define CAN_TAB_STAT 22
-#define CAN_BATT1_STAT 23
-#define CAN_PMB1_STAT 24
-#define CAN_BATT2_STAT 25
-#define CAN_PMB2_STAT 26
-#define CAN_TAB_ERROR 32
-#define CAN_STB_ERROR 33
-#define CAN_PMB1_ERROR 34
-#define CAN_PMB2_ERROR 35
-#define CAN_SBCCAN_ERROR 36
+#define CAN_ALL_CONFIG 0xA
+#define CAN_STB_SENS 0xB
+#define CAN_STB_UP 0xC
+#define CAN_STB_GYRO 0xD
+#define CAN_STB_ACC 0xE
+#define CAN_STB_MAG 0xF
+#define CAN_STB_STAT 0x10
+#define CAN_THT_STAT_1 0x11
+#define CAN_THT_STAT_2 0x12
+#define CAN_THT_PWM_1 0x13
+#define CAN_THT_PWM_2 0x14
+#define CAN_ACT_STAT 0x15
+#define CAN_TAB_STAT 0x16
+#define CAN_BATT1_STAT 0x17
+#define CAN_PMB1_STAT 0x18
+#define CAN_BATT2_STAT 0x19
+#define CAN_PMB2_STAT 0x1A
+#define CAN_TAB_ERROR 0x20
+#define CAN_STB_ERROR 0x21
+#define CAN_PMB1_ERROR 0x22
+#define CAN_PMB2_ERROR 0x23
+#define CAN_SBCCAN_ERROR 0x24
 
 //CAN Heartbeat
-#define HEARTBEAT_SA 0x01
-#define HEARTBEAT_Thruster 0x02
-#define HEARTBEAT_Backplane 0x03
-#define HEARTBEAT_SBC_CAN 0x04
-#define HEARTBEAT_SBC 0x05
-#define HEARTBEAT_PMB1 0x06
-#define HEARTBEAT_PMB2 0x07
-#define HEARTBEAT_Telemetry 0x08
+#define HEARTBEAT_SBC 0x01
+#define HEARTBEAT_SBC_CAN 0x02
+#define HEARTBEAT_PCB 0x03
+#define HEARTBEAT_Thruster_Board 0x04
+#define HEARTBEAT_STB 0x05
+#define HEARTBEAT_Manipulator 0x06
+#define HEARTBEAT_PMB1 0x07
+#define HEARTBEAT_PMB2 0x08
 
  void publishCAN_message(void);
 
