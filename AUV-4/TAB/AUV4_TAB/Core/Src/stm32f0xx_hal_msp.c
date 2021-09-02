9e6f7795e2b8c58eb296a81aa2e7933623d93720
n_yj <nyj.161@gmail.com>
2021-09-03 03:33:29 +0800

    fix missing thruster 3 PWM

--- AUV-4/TAB/AUV4_TAB/Core/Src/stm32f0xx_hal_msp.c
+++ AUV-4/TAB/AUV4_TAB/Core/Src/stm32f0xx_hal_msp.c
@@ -192,9 +192,9 @@ void HAL_TIM_MspPostInit(TIM_HandleTypeDef* htim)
     PA1     ------> TIM2_CH2
     PA2     ------> TIM2_CH3
     PA3     ------> TIM2_CH4
-    PA5     ------> TIM2_CH1 
+    PA0     ------> TIM2_CH1
     */
-    GPIO_InitStruct.Pin = GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_5;
+    GPIO_InitStruct.Pin = GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_0;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
