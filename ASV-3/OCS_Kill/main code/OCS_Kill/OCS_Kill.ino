9f7ba3b18ba2c9428877165f491244e0a93b437f
teohxuen <projectxuen@gmail.com>
2022-10-04 11:53:17 +0800

    Initial Commit for OCS Kill

---
+++ ASV-3/OCS_Kill/main code/OCS_Kill/OCS_Kill.ino
@@ -0,0 +1,46 @@
+#include "n2420.h"
+#include <SoftwareSerial.h>
+
+// create the N2420 object
+N2420 n2420 (OCS_EXTENSION);
+int payload[] = { 0 };
+
+int8_t sentByte = 0x15;
+uint8_t inByte;
+static uint32_t radio_loop = 0;
+
+SoftwareSerial mySerial(6, 7); // RX, TX
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200); 
+  mySerial.begin(115200); 
+  n2420.setSerial(&mySerial);
+
+  pinMode(3, INPUT);
+  Serial.println("Restart");
+}
+
+void loop() {
+
+  if ((millis() - radio_loop) > 300){
+  
+    if (digitalRead(3)== HIGH){
+      sentByte = 0x15;
+      Serial.println("normal");       //Normal Operations when HIGH
+    }
+    else{
+      sentByte = 0x44;                
+      Serial.println("KILL");         //KILL Operations when LOW
+    }
+    
+    Serial.print("sendbyte: ");
+    Serial.println(sentByte, HEX);
+    payload[0] = sentByte;
+    Serial.print("payload: ");
+    Serial.println(payload[0], HEX);
+    n2420.writePacket(payload,1);
+    radio_loop = millis();
+  }
+
+}
